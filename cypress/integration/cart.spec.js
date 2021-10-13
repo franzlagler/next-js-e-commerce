9f5b4c0c004528d7f1894d1d5af4c4cb62eac757
Franz Lagler <franz.lagler@outlook.com>
2021-10-13 17:38:45 +0200

    Change test files

--- cypress/integration/cart.spec.js
+++ cypress/integration/cart.spec.js
@@ -13,7 +13,7 @@ const deleteProduct = (index) => {
 
 describe('Cart', () => {
   it('should be able to add items to cart and delete them', () => {
-    cy.visit(`http://localhost:3000/`);
+    cy.visit('http://localhost:3000/');
     /*  addProduct();
     addProduct();
 
