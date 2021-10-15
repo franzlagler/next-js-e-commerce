ee96019d495abb6587c8072831a6947338adf4d7
Franz Lagler <franz.lagler@outlook.com>
2021-10-15 21:55:46 +0200

    update jest files variables

--- util/__tests/price.test.js
+++ util/__tests/price.test.js
@@ -5,11 +5,11 @@ const sampleCart = [
   { id: 5, amount: 8 },
 ];
 const sampleProducts = [
-  { productsId: 1, name: 'test', price: 4.5 },
-  { productsId: 2, name: 'test', price: 2.8 },
-  { productsId: 3, name: 'test', price: 1.5 },
-  { productsId: 4, name: 'test', price: 7.9 },
-  { productsId: 5, name: 'test', price: 3.7 },
+  { productId: 1, name: 'test', price: 4.5 },
+  { productId: 2, name: 'test', price: 2.8 },
+  { productId: 3, name: 'test', price: 1.5 },
+  { productId: 4, name: 'test', price: 7.9 },
+  { productId: 5, name: 'test', price: 3.7 },
 ];
 
 test('calculates price correctly depending on data', () => {
